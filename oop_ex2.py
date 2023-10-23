29b43b0e72e2133d5ba56903d7aa4d940e66a900
rafafilm01 <rafafilm01@hotmail.com>
2023-10-23 16:26:17 +0200

    new class added

--- oop_ex2.py
+++ oop_ex2.py
@@ -40,4 +40,20 @@ class Group:
         
     def get_members (self):
         return sorted(self.members)
+    
+    # method that adds a new group of members  to the existing group and returns the merged list 
+    def merge(self, group):
+        combined_members = self.members + group.members
+        new_group =Group("any name", combined_members)
+        return new_group
         
+    #explanation 
+    # under new_group we launch another instance of class Group , this time with the combined members from 2 groups. Example of prompts 
+    
+#creating objects based on the Group class and accessing them members 
+g1 =Group("a-team", ['raf', 'roger'])
+g2 =Group('b-team', ['brian', 'steven'])
+g3 =g1.merge(g2)
+print(g3.get_members())
+print(type(g3))
+    
